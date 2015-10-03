db6267f2ccf996e2a49378f61403ab7965bb7adf
Richard Stanley <richard@thrillride.local>
2015-10-03 19:11:24 +0300

    fabfile auto-commit

--- telephony/telephony.py
+++ telephony/telephony.py
@@ -30,4 +30,3 @@ def hello():
 
 if __name__ == "__main__":
     application.run(debug=False, host='0.0.0.0')
-    application.run(debug=False)
--- telephony/wsgi.py
+++ telephony/wsgi.py
@@ -1,4 +1,5 @@
 from telephony import application
 
 if __name__ == "__main__":
-    application.run()
+    #application.run()
+    application.run(debug=False, host='0.0.0.0')
