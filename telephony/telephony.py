e6336042a3fa037535eda95d3f9022d61ee0c2c3
Richard Stanley <richard@thrillride.local>
2015-10-03 18:45:51 +0300

    fabfile auto-commit

--- telephony/telephony.py
+++ telephony/telephony.py
@@ -29,5 +29,5 @@ def hello():
     return vxml_xml2
 
 if __name__ == "__main__":
-    #application.run(debug=False, host='0.0.0.0')
+    application.run(debug=False, host='0.0.0.0')
     application.run(debug=False)
