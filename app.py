e704890a4177948e7c0ad94809cb30e39a97bcf2
Richard Stanley <richard@thrillride.local>
2015-12-13 17:40:41 +0300

    fabfile auto-commit

--- app.py
+++ app.py
@@ -6,29 +6,29 @@ application = Flask(__name__)
 def hello():
     return "<h1 style='color:blue'>flaskin like a baw$$</h1>"
 
-@application.route("/twilio", methods=['GET', 'POST'])
-def twilio():
-    resp = twiml.Response()
-    resp.say("Welcome to twilio!")
-    print(str(resp))
-
-
-vxml_xml2 = """
-<?xml version="1.0" encoding="UTF-8"?><vxml version="2.1">
-<form>
-<block>
-<prompt xml:lang="en-in-female">
-<break time="2s"/>
-Welcome to You Report! Please tell us what district you live in.
-</prompt>
-</block>
-</form>
-</vxml>
-"""
-
-@application.route("/nexmo")
-def nexmo():
-    return vxml_xml2
+# @application.route("/twilio", methods=['GET', 'POST'])
+# def twilio():
+#     resp = twiml.Response()
+#     resp.say("Welcome to twilio!")
+#     print(str(resp))
+#
+#
+# vxml_xml2 = """
+# <?xml version="1.0" encoding="UTF-8"?><vxml version="2.1">
+# <form>
+# <block>
+# <prompt xml:lang="en-in-female">
+# <break time="2s"/>
+# Welcome to You Report! Please tell us what district you live in.
+# </prompt>
+# </block>
+# </form>
+# </vxml>
+# """
+#
+# @application.route("/nexmo")
+# def nexmo():
+#     return vxml_xml2
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 from app import application
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    application.run(host='0.0.0.0', port='8000')
     #application.run(debug=False)
